f93396316b7eb46ddda0710fd16ad1e71b4e51c5
jinchen1036 <42017125+jinchen1036@users.noreply.github.com>
2018-08-09 00:43:35 -0400

    Add files via upload

--- TipCal/SettingViewController.swift
+++ TipCal/SettingViewController.swift
@@ -8,34 +8,63 @@
 
 import UIKit
 
-class SettingViewController: UIViewController {
+class SettingViewController: UIViewController, UIPickerViewDelegate,UIPickerViewDataSource {
 
-    @IBOutlet weak var TipControl: UISegmentedControl!
+    @IBOutlet weak var tip1: UILabel!
+    @IBOutlet weak var tip2: UILabel!
+    @IBOutlet weak var tip3: UILabel!
+    @IBOutlet weak var stepper1: UIStepper!
+    @IBOutlet weak var stepper2: UIStepper!
+    @IBOutlet weak var stepper3: UIStepper!
+    @IBOutlet weak var numPicker: UIPickerView!
+    
+    var nums = Array(1...20)  // 20 Selection for Number to Split
+    var numSplit = 1          // Initial default Number to Split
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
     
-    @IBAction func TipSet(_ sender: Any) {
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return String(nums[row])
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return nums.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        
+        numSplit = nums[row]
+        self.Changes(self)
+        print(nums[row])
+    }
+
+    
+    @IBAction func Changes(_ sender: Any) {
         let defaults = UserDefaults.standard
-        defaults.set(TipControl.selectedSegmentIndex, forKey: "TipSel")
+        
+        // Update Labels Text
+        tip1.text = String(Int(stepper1.value))
+        tip2.text = String(Int(stepper2.value))
+        tip3.text = String(Int(stepper3.value))
+
+        // Update Value into Default Keys
+        defaults.set(Int(stepper1.value), forKey: "tip1")
+        defaults.set(Int(stepper2.value), forKey: "tip2")
+        defaults.set(Int(stepper3.value), forKey: "tip3")
+        defaults.set(numSplit, forKey: "numSplit")
         defaults.synchronize()
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
--- TipCal/ViewController.swift
+++ TipCal/ViewController.swift
@@ -17,14 +17,17 @@ class ViewController: UIViewController {
     @IBOutlet weak var p2Label: UILabel!
     @IBOutlet weak var p3Label: UILabel!
     @IBOutlet weak var p4Label: UILabel!
+    @IBOutlet weak var defaultNum: UILabel!
+    @IBOutlet weak var defaultValue: UILabel!
     
+    var tipPers: [Int] = [15, 20, 25]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        BillField.backgroundColor = UIColor(red: 153/255, green: 255/255, blue: 255/255, alpha: 1)
-        BillField.resignFirstResponder()
+       // BillField.backgroundColor = UIColor(red: 153/255, green: 255/255, blue: 255/255, alpha: 1)
+        //BillField.resignFirstResponder()
     }
 
     override func didReceiveMemoryWarning() {
@@ -34,22 +37,37 @@ class ViewController: UIViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        // update the tip amount
+        
+        // update the variable from setting page
         let defaults = UserDefaults.standard
-        TipControl.selectedSegmentIndex = defaults.integer(forKey: "TipSel")
+        tipPers[0] = defaults.integer(forKey: "tip1")
+        tipPers[1] = defaults.integer(forKey: "tip2")
+        tipPers[2] = defaults.integer(forKey: "tip3")
+        
+        TipControl.setTitle("\(tipPers[0])"+"%", forSegmentAt: 0)
+        TipControl.setTitle("\(tipPers[1])"+"%", forSegmentAt: 1)
+        TipControl.setTitle("\(tipPers[2])"+"%", forSegmentAt: 2)
+        
+        defaultNum.text = String(defaults.integer(forKey: "numSplit"))
+        //print("Change", tipPers)
         Calculate(self)
     }
     
     
     @IBAction func Calculate(_ sender: Any) {
-        let tipPer = [0.18, 0.20, 0.25][TipControl.selectedSegmentIndex] // tip percentage
+        //print("Original", tipPers)
+        
+        let tipPer = Double(tipPers[TipControl.selectedSegmentIndex])/100 // tip percentage
+        //print(tipPer)
         let bill = Double(BillField.text!) ?? 0 // Get Bill Value
         
-        let tip = bill * tipPer // Get Tip Value
+        let tip = bill * tipPer  // Get Tip Value
         let p1 = bill + tip
         let p2 = p1/2
         let p3 = p1/3
         let p4 = p1/4
+        let numSplit = Double(defaultNum.text!) ?? 1
+        let p5 = p1/numSplit
         
         // Set format of locale currency
         let formatter = NumberFormatter()
@@ -62,10 +80,29 @@ class ViewController: UIViewController {
         p2Label.text  = formatter.string(from: p2 as NSNumber)
         p3Label.text  = formatter.string(from: p3 as NSNumber)
         p4Label.text  = formatter.string(from: p4 as NSNumber)
+        defaultValue.text = formatter.string(from: p5 as NSNumber)
         
-     }
+        // Change Color For diffenent Tip Percentage
+        if TipControl.selectedSegmentIndex == 0 {
+            self.view.backgroundColor = UIColor(red:0.00, green:0.87, blue:0.84, alpha:1.0)
+            BillField.backgroundColor = UIColor(red:0.66, green:0.96, blue:0.95, alpha:1.0)
+        }
+        else if TipControl.selectedSegmentIndex == 1 {
+            self.view.backgroundColor = UIColor(red:1.00, green:0.00, blue:1.00, alpha:1.0)
+            BillField.backgroundColor = UIColor(red: 0.95, green: 0.72, blue: 0.94, alpha: 1.0)
+            
+        }
+        else {
+            self.view.backgroundColor = UIColor(red:0.00, green:0.87, blue:0.45, alpha:1.0)
+            BillField.backgroundColor = UIColor(red:0.35, green:0.98, blue:0.51, alpha:1.0)
+            
+        }
+        
+    }
+     
     
-    @IBAction func Keyboard(_ sender: Any) {
+    @IBAction func Keyboard(_ sender: Any)
+    {
         view.endEditing(true) // Dismiss Keyboard
     }
     
